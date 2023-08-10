92498e55976c8aa5e8f983ebd6f7f325b38e405c
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-08-10 20:29:21 +0100

    Remove some comments

--- tournament_winner.py
+++ tournament_winner.py
@@ -1,7 +1,4 @@
 def tournament_winner(competitions, results):
-    # Initiate an empty dictionary for the final scores
-    # Loop through competitions and populate the final scores dictionary
-    # Loop through the dictionary to pick out the tournament winner
     final_scores = {}
     for index in range(len(results)):
         competition = competitions[index]
@@ -10,4 +7,12 @@ def tournament_winner(competitions, results):
         if winning_team not in final_scores:
             final_scores[winning_team] = 0
         final_scores[winning_team] += 3
-    pass
+
+    highest_score = 0
+    winner = None
+    for team in final_scores:
+        if winner is None or final_scores[team] > highest_score:
+            winner = team
+            highest_score = final_scores[team]
+
+    return winner
