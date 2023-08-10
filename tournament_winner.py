010c7c2c965bd1632c01cdedaa332789e89c087e
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-08-10 20:25:11 +0100

    Get final scores dictionary

--- tournament_winner.py
+++ tournament_winner.py
@@ -1,2 +1,13 @@
 def tournament_winner(competitions, results):
+    # Initiate an empty dictionary for the final scores
+    # Loop through competitions and populate the final scores dictionary
+    # Loop through the dictionary to pick out the tournament winner
+    final_scores = {}
+    for index in range(len(results)):
+        competition = competitions[index]
+        result = results[index]
+        winning_team = competition[1] if result == 0 else competition[0]
+        if winning_team not in final_scores:
+            final_scores[winning_team] = 0
+        final_scores[winning_team] += 3
     pass
